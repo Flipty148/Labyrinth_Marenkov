66bd5c0dbfd79553c6123db87e51a6652529a739
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-29 09:56:19 +0300

    Добавлена заготовка класса LabyrinthCreator
    Добавлена заготовка класса LabyrinthCreator, который служит для создания лабиринта.

---
+++ Assets/Scripts/LabyrinthCreator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject Cell;
+}
