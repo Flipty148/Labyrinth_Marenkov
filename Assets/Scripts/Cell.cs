6c90adc7894d6a9e643dc6784eb19809f1663b5c
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-29 12:01:03 +0300

    В класс Cell добавлен метод SetWallsActive, который устанавливает активность соответствующих стен

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,4 +6,12 @@ public class Cell : MonoBehaviour
     [SerializeField] private GameObject RightWall;
     [SerializeField] private GameObject UpperWall;
     [SerializeField] private GameObject BottomWall;
+
+    public void SetWallsActive(bool LeftActive, bool RightActive, bool UpperActive, bool BottomActive)
+    {
+        LeftWall.SetActive(LeftActive);
+        RightWall.SetActive(RightActive);
+        UpperWall.SetActive(UpperActive);
+        BottomWall.SetActive(BottomActive);
+    }
 }
