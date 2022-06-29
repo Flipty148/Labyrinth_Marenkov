95b8e2272675329da828dce40a1f9dec0a8a7b92
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-29 12:30:53 +0300

    Добавлено отображение лабиринта на сцене unity для класса  LabyrinthCreator

--- Assets/Scripts/LabyrinthCreator.cs
+++ Assets/Scripts/LabyrinthCreator.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class LabyrinthCreator : MonoBehaviour
 {
-    [SerializeField] private GameObject Cell;
+    [SerializeField] private GameObject Cell; //Ячейка лабиринта
+    [SerializeField] private int Width = 5; //Ширина лабиринта
+    [SerializeField] private int Height = 5; //Высота лабиринта
+
+
+    private void Start()
+    {
+        CreateLabyrinth(); //Создать лабиринт
+    }
+
+    private void CreateLabyrinth()
+    {
+        GameObject Cells = new GameObject("Cells"); //Объект для всех ячеек
+        LabyrinthGenerator generator = new LabyrinthGenerator(Width, Height); //Генератор лабиринта 
+        LabyrinthCell[,] labyrinth = generator.GenerateLabyrinth(); //Генерация лабиринта
+
+        //Отображение лабиринта на сцене
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                GameObject curObject = Instantiate(Cell, new Vector2(x, y), Quaternion.identity); //Создать ячейку
+                curObject.transform.SetParent(Cells.transform); //Добавить текущую ячейку ко всем ячейкам
+                Cell cell = curObject.GetComponent<Cell>();
+                cell.SetWallsActive(labyrinth[x, y].LeftWall, labyrinth[x, y].RightWall, labyrinth[x, y].UpperWall, labyrinth[x, y].BottomWall); //Изменить видимость стен в ячейке
+            }
+        }
+    }
 }
