3862d324f91f97914839da8b1721c28e2f6cf4c2
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-06-29 11:52:16 +0300

    Добавлен класс Cell
    Добавлен класс Cell, содержащий ссылки на соответствующие стены, необходим для LabyrinthCreator для изменения отображения стен на сцене.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private GameObject LeftWall;
+    [SerializeField] private GameObject RightWall;
+    [SerializeField] private GameObject UpperWall;
+    [SerializeField] private GameObject BottomWall;
+}
